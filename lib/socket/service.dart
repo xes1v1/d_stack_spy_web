1c8c5e66d85562ac98808d5e0de6064f768e4c53
maxu <maxu@tal.com>
2020-12-12 15:02:31 +0800

    增加动态获取地址逻辑

--- lib/socket/service.dart
+++ lib/socket/service.dart
@@ -1,15 +1,15 @@
 import 'dart:convert';
+import 'dart:convert' as convert;
 
 import 'package:dstack_spy_web/model/image_entity.dart';
 import 'package:dstack_spy_web/model/node_entity.dart';
 import 'package:dstack_spy_web/socket/message_handler.dart';
+import 'package:http/http.dart' as http;
 
 import 'connect.dart';
 
 /// Created by shiyucheng on 2020/12/8
 class Service {
-  static const String serviceUrl = 'ws://10.29.118.14:4041/ws';
-
   Service._();
 
   static Service _instance;
@@ -23,12 +23,26 @@ class Service {
 
   SocketConnect connector;
 
-  void initConnect() {
-    connector = SocketConnect(serviceUrl, receiver: _receiver);
+  Future<void> start() async {
+    var url = 'http://localhost:9202/getHost';
+    var response = await http.get(url);
+    if (response.statusCode == 200) {
+      Map<String, dynamic> jsonResponse = convert.jsonDecode(response.body);
+      String host = jsonResponse['host'];
+      int port = jsonResponse['port'];
+      if (host == null || host.isEmpty) {
+        return;
+      }
+      initConnect(host, port);
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
   }
 
-  void start() {
-    initConnect();
+  void initConnect(String host, int port) {
+    String serviceUrl = 'ws://$host:$port/ws';
+    print('webSocket地址$serviceUrl');
+    connector = SocketConnect(serviceUrl, receiver: _receiver);
     connector.connect();
     Map map = {'socketClient': 'html'};
     var msg = jsonEncode(map);
